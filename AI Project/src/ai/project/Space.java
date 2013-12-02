6f3195d15b0fc3fa513ca9c899d8525205bcb30f
Adam <Adam@PC-Adam>
2013-12-02 01:42:18 +0100

    Criado Classe Space

---
+++ AI Project/src/ai/project/Space.java
@@ -0,0 +1,68 @@
+/**
+ * @author Adam Silva and Miguel Silva
+ */
+
+package ai.project;
+
+public class Space {
+    
+    // TRUE = SPACE
+    // FALSE = WALL
+    private boolean goUp = false;
+    private boolean goDown = false;
+    private boolean goLeft = false;
+    private boolean goRight = false;
+    private int spaceNumber = 0;
+    private int line = 0; 
+    private int column = 0; 
+
+    public Space(boolean goUp, boolean goLeft, boolean goDown, boolean goRight, int line, int column) 
+    {
+        this.goUp = goUp;
+        this.goLeft = goLeft;
+        this.goDown = goDown;
+        this.goRight = goRight;
+        this.line = line;
+        this.column = column;
+    }
+
+    public boolean hasWallDown() 
+    {
+        return goDown;
+    }
+
+    public boolean hasWallLeft() 
+    {
+        return goLeft;
+    }
+
+    public boolean hasWallRight() 
+    {
+        return goRight;
+    }
+
+    public boolean hasWallUp() 
+    {
+        return goUp;
+    }
+    
+    public int getSpaceNumber()
+    {
+        return spaceNumber;
+    }
+    
+    public void setSpaceNumber(int spaceNumber)
+    {
+        this.spaceNumber = spaceNumber;
+    }
+    
+    public int getLine()
+    {
+        return line;
+    }
+    
+    public int getColumn()
+    {
+        return column;
+    }
+}
