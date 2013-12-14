32818dc963d2189ed0ba7930989b3b3f18839ca4
Adam <Adam@PC-Adam>
2013-12-14 19:37:39 +0100

    Adicionado os metodos de pesquisa BFS(queue) e DFS(stack). e alterado o nome de alguns metodos na classe Space

--- AI Project/src/ai/project/AIProject.java
+++ AI Project/src/ai/project/AIProject.java
@@ -6,6 +6,12 @@
 
 package ai.project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author Miguel
@@ -16,7 +22,31 @@ public class AIProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //dsvbsdybsdhibcvusd
+        Maze maze = new Maze();
+//        JFileChooser fc = new JFileChooser();
+//        int returnVal = fc.showOpenDialog(null);
+//
+//        if (returnVal == JFileChooser.APPROVE_OPTION) {
+//            File file = fc.getSelectedFile();
+//            String path = file.getAbsolutePath();
+//            if (path.endsWith(".txt")) {
+//                try {
+//                    maze.readFile(path);
+//                } catch (FileNotFoundException ex) {
+//                    Logger.getLogger(AIProject.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//        }
+        try {
+            maze.readFile("C:\\Users\\Adam\\Documents\\AI-Project\\testing normal maze.txt");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AIProject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        maze.methodBFS();
+        maze.resetMaze();
+        System.out.println("_______________________________________");
+        
+        maze.methodDFS();
     }
     
 }
--- AI Project/src/ai/project/Maze.java
+++ AI Project/src/ai/project/Maze.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class Maze {
 
     private Space maze[][];
+    
     private int begin[] = new int[2];
     private int end[] = new int[2];
 
@@ -18,7 +19,7 @@ public class Maze {
         Scanner in = new Scanner(new File(filepath));
 
         //Reads the file, and saves each line as a String in an ArrayList
-        ArrayList<String> text = new ArrayList<String>();
+        ArrayList<String> text = new ArrayList<>();
         while (in.hasNextLine()) {
             text.add(in.nextLine());
         }
@@ -36,7 +37,7 @@ public class Maze {
         int nrLines = (text.size() - 3) / 2;
         maze = new Space[nrLines][nrColumns];
 
-        //Creates every Space for the Maze checking around each one for it's viable directions, checking for barriers and walls, seeing where it's possible to go from there. If direction is true, it's possible.
+        //Creates every Space for the Maze checking around each one for it's viable directions, checking for barriers and walls, seeing where it's possible to go from there. If direction is true, it's possible
         boolean up, right, left, down;
         int line = 1, column;
         for (int i = 0; i < nrLines; i++) {
@@ -57,10 +58,143 @@ public class Maze {
                     left = false;
                 }
 
-                maze[i][i] = new Space(up, left, down, right, i, j);
+                maze[i][j] = new Space(up, left, down, right, i, j);
                 column = column + 2;
             }
             line = line + 2;
         }
+    }
+    
+    public void resetMaze() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                maze[i][j].setSpaceNumber(0);
+            }
+        }
+    }
+    
+    //Right->Down->Left->Up
+    public void methodDFS(){
+        long startTime = System.nanoTime();    
+
+        
+        Stack <Space> stackDFS = new Stack();
+        int count=0;
+        boolean nextMove = true;
+        Space currentSpace = maze[begin[0]][begin[1]];
+        Space nextOne;
+        
+        while(nextMove){
+            nextMove=false;
+            if(currentSpace.hasSpaceRight()==true && maze[currentSpace.getLine()][currentSpace.getColumn()+1].getSpaceNumber()==0){
+                //System.out.println("RIGHT = "+currentSpace.getLine()+","+currentSpace.getColumn());
+                nextOne = maze[currentSpace.getLine()][currentSpace.getColumn()+1];
+                stackDFS.addElement(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceDown()==true && maze[currentSpace.getLine()+1][currentSpace.getColumn()].getSpaceNumber()==0){
+                //System.out.println("DOWN = "+currentSpace.getLine()+","+currentSpace.getColumn());
+                nextOne = maze[currentSpace.getLine()+1][currentSpace.getColumn()];
+                stackDFS.addElement(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceLeft()==true && maze[currentSpace.getLine()][currentSpace.getColumn()-1].getSpaceNumber()==0){
+                //System.out.println("LEFT = "+currentSpace.getLine()+","+currentSpace.getColumn());
+                nextOne = maze[currentSpace.getLine()][currentSpace.getColumn()-1];
+                stackDFS.addElement(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceUp()==true && maze[currentSpace.getLine()-1][currentSpace.getColumn()].getSpaceNumber()==0){
+                //System.out.println("UP = "+currentSpace.getLine()+","+currentSpace.getColumn());
+                nextOne = maze[currentSpace.getLine()-1][currentSpace.getColumn()];
+                stackDFS.addElement(nextOne);
+                nextMove=true;
+            }
+            
+            count++;
+            maze[currentSpace.getLine()][currentSpace.getColumn()].setSpaceNumber(count);
+            //System.out.println("CurrentSpace: "+maze[currentSpace.getLine()][currentSpace.getColumn()].getSpaceNumber());
+            
+            if (!stackDFS.isEmpty()) {
+                do {
+                    if (stackDFS.peek().getSpaceNumber() != 0) {
+                        stackDFS.pop();
+                    }
+                } while (!stackDFS.isEmpty() && stackDFS.peek().getSpaceNumber() != 0);
+                if (!stackDFS.isEmpty()) {
+                    currentSpace = stackDFS.pop();
+                    nextMove = true;
+                }
+            }
+        }  
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("DFS-Elapsed Time:"+elapsedTime);
+    }
+    
+    //Up->Left->Down->Right
+    public void methodBFS(){
+        long startTime = System.nanoTime();
+        Queue <Space> queueBFS = new LinkedList();
+        int count=0;
+        boolean nextMove = true;
+        Space currentSpace = maze[begin[0]][begin[1]];
+        Space nextOne;
+        
+        while(nextMove){
+            nextMove=false;
+            if(currentSpace.hasSpaceUp()==true && maze[currentSpace.getLine() - 1][currentSpace.getColumn()].getSpaceNumber()==0){
+                
+                nextOne = maze[currentSpace.getLine()-1][currentSpace.getColumn()];
+                queueBFS.add(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceLeft()==true && maze[currentSpace.getLine()][currentSpace.getColumn()-1].getSpaceNumber()==0){
+                
+                nextOne = maze[currentSpace.getLine()][currentSpace.getColumn()-1];
+                queueBFS.add(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceDown()==true && maze[currentSpace.getLine()+1][currentSpace.getColumn()].getSpaceNumber()==0){
+                
+                nextOne = maze[currentSpace.getLine()+1][currentSpace.getColumn()];
+                queueBFS.add(nextOne);
+                nextMove=true;
+            }
+            if(currentSpace.hasSpaceRight()==true && maze[currentSpace.getLine()][currentSpace.getColumn()+1].getSpaceNumber()==0){
+                
+                nextOne = maze[currentSpace.getLine()][currentSpace.getColumn()+1];
+                queueBFS.add(nextOne);
+                nextMove=true;
+            }
+            count++;
+            maze[currentSpace.getLine()][currentSpace.getColumn()].setSpaceNumber(count);
+            //System.out.println("CurrentSpace: "+maze[currentSpace.getLine()][currentSpace.getColumn()].getSpaceNumber());
+            //printQueue(queueBFS);
+            
+//            currentSpace = queueBFS.poll();
+            
+            
+            if (!queueBFS.isEmpty()) {
+                do {
+                    if (queueBFS.peek().getSpaceNumber() != 0) {
+                        queueBFS.poll();
+                    }
+                } while (!queueBFS.isEmpty() && queueBFS.peek().getSpaceNumber() != 0);
+                if (!queueBFS.isEmpty()) {
+                    currentSpace = queueBFS.poll();
+                    nextMove = true;
+                }
+            }
+            
+        }
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("BFS-Elapsed Time:"+elapsedTime);
+    }
+    
+    public void printQueue(Queue<Space> queue){
+        for(Space space:queue){
+            System.out.print(space.getSpaceNumber()+",");
+        }
+        System.out.println();
     }
 }
